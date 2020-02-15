c322c656cb64c661db31684ddceece19c6081cb6
Kevin Friedl <kevin.friedl@adorsys.de>
2020-02-15 20:46:26 +0100

    add start record timer and ui

--- lib/screen/createVideo.dart
+++ lib/screen/createVideo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -7,35 +9,117 @@ class CreateVideoScreen extends StatefulWidget {
 }
 
 class _CreateVideoScreenState extends State<CreateVideoScreen> {
-  CameraController controller;
+  CameraController _cameraController;
+  bool _cameraGranted = false;
+  double _sliderProgress = 0;
+  bool _recordState = true;
+  Timer _recodringTimer;
 
-  @override
-  void initState() {
-    super.initState();
+  _accessCamera() {
     availableCameras().then((cameras) {
-      controller = CameraController(cameras[0], ResolutionPreset.medium);
-      controller.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {});
+      _cameraController = CameraController(cameras[1], ResolutionPreset.medium);
+      if (_cameraController != null)
+        _cameraController.initialize().then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {
+            _cameraGranted = true;
+          });
+        });
+    });
+  }
+
+  _startRecording() {
+    if (_recodringTimer == null)
+      _recodringTimer = Timer.periodic(Duration(milliseconds: 60), (timer) {
+        if (_sliderProgress < .999)
+          setState(() {
+            _sliderProgress += .001;
+            _recordState = false;
+          });
+        else
+          _stopRecording();
       });
+  }
+
+  _stopRecording() {
+    setState(() {
+      _recodringTimer.cancel();
+      _recodringTimer = null;
+      _sliderProgress = 0;
+      _recordState = true;
     });
   }
 
   @override
   void dispose() {
-    controller?.dispose();
+    _cameraController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (controller == null) {
-      return Container();
-    }
-    return AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: CameraPreview(controller));
+    return Scaffold(
+      body: (!_cameraGranted)
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Please allow access to your camera and microphone"),
+                  FlatButton(
+                    child: Text("Allow"),
+                    onPressed: () {
+                      _accessCamera();
+                    },
+                  )
+                ],
+              ),
+            )
+          : Stack(
+              children: <Widget>[
+                Transform.scale(
+                  scale: _cameraController.value.aspectRatio /
+                      (MediaQuery.of(context).size.width /
+                          MediaQuery.of(context).size.height),
+                  child: Center(
+                    child: AspectRatio(
+                      aspectRatio: _cameraController.value.aspectRatio,
+                      child: CameraPreview(_cameraController),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0, .6),
+                  child: Text("Time remaining: " +
+                      (60 - (_sliderProgress * 60)).toInt().toString()),
+                ),
+                Align(
+                  alignment: Alignment(0, .7),
+                  child: SizedBox(
+                      height: 20,
+                      child: Slider(
+                        value: _sliderProgress,
+                        onChanged: (_) {},
+                      )),
+                ),
+                Align(
+                  alignment: Alignment(0, .9),
+                  child: FloatingActionButton(
+                    child: Icon((_recordState)
+                        ? Icons.fiber_manual_record
+                        : Icons.stop),
+                    onPressed: () {
+                      if (_recordState)
+                        _startRecording();
+                      else
+                        _stopRecording();
+                    },
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+    );
   }
 }
