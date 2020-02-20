be7d9d9f510adcd9034a3c5d86f78daf3913c5a4
Kevin Friedl <kevin.friedl@adorsys.de>
2020-02-20 17:56:29 +0100

    add location dropdown to login

--- lib/screen/createProfile.dart
+++ lib/screen/createProfile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_match/screen/homeScreen.dart';
 import 'package:video_match/utils/colors.dart';
+import 'package:video_match/utils/country_states.dart';
 
 class CreateProfile extends StatefulWidget {
   @override
@@ -11,9 +12,37 @@ class _CreateProfileState extends State<CreateProfile> {
   PageController _pageController = PageController();
   TextEditingController _textEditingControllerFirstName =
       TextEditingController();
+  TextEditingController _textEditingControllerMinAge =
+      TextEditingController(text: "18");
+  TextEditingController _textEditingControllerMaxAge =
+      TextEditingController(text: "80");
   DateTime _age;
   bool _gender = true;
   bool _genderLockingFor = false;
+  String selectedCountry, selectedState;
+  List<String> countries = List<String>();
+  List<String> states = List<String>();
+
+  List<String> statesOfCountrie(String country) {
+    List<String> statesTemp = List<String>();
+
+    country_states["countries"].forEach((c) {
+      if (c["country"] == country) statesTemp = c["states"];
+    });
+
+    return statesTemp;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCountry = "None";
+    selectedState = "-";
+    country_states["countries"].forEach((c) {
+      countries.add(c["country"]);
+    });
+    states = statesOfCountrie(selectedCountry);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,16 +59,19 @@ class _CreateProfileState extends State<CreateProfile> {
                   Text(
                     "Please enter your first name:",
                   ),
-                  TextField(
-                    controller: _textEditingControllerFirstName,
-                    autofocus: true,
-                    maxLength: 20,
-                    style: TextStyle(color: Colors.white),
-                    onSubmitted: (_) {
-                      _pageController.nextPage(
-                          duration: Duration(milliseconds: 200),
-                          curve: Curves.decelerate);
-                    },
+                  Container(
+                    width: 200,
+                    child: TextField(
+                      controller: _textEditingControllerFirstName,
+                      maxLength: 20,
+                      style: TextStyle(color: Colors.white),
+                      onSubmitted: (_) {
+                        _pageController.nextPage(
+                            duration: Duration(milliseconds: 200),
+                            curve: Curves.decelerate);
+                      },
+                      textAlign: TextAlign.center,
+                    ),
                   )
                 ],
               ),
@@ -99,6 +131,46 @@ class _CreateProfileState extends State<CreateProfile> {
               InnerPageUserData(
                 children: <Widget>[
                   Text(
+                    "Please select your country and staate:",
+                  ),
+                  DropdownButton<String>(
+                    style: TextStyle(color: Colors.white),
+                    value: selectedCountry,
+                    onChanged: (String newCountry) {
+                      setState(() {
+                        selectedCountry = newCountry;
+                        states = statesOfCountrie(newCountry);
+                        selectedState = states.first;
+                      });
+                    },
+                    items:
+                        countries.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  DropdownButton<String>(
+                    style: TextStyle(color: Colors.white),
+                    value: selectedState,
+                    onChanged: (String newState) {
+                      setState(() {
+                        selectedState = newState;
+                      });
+                    },
+                    items: states.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              InnerPageUserData(
+                children: <Widget>[
+                  Text(
                     "Please select your gender you are looking for:",
                   ),
                   Row(
@@ -123,6 +195,56 @@ class _CreateProfileState extends State<CreateProfile> {
                   )
                 ],
               ),
+              InnerPageUserData(
+                children: <Widget>[
+                  Text(
+                    "Please enter the age range of the person you are looking for:",
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Flexible(
+                          child: TextField(
+                            controller: _textEditingControllerMinAge,
+                            keyboardType: TextInputType.number,
+                            maxLength: 2,
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Text("-"),
+                        Flexible(
+                          child: TextField(
+                            controller: _textEditingControllerMaxAge,
+                            keyboardType: TextInputType.number,
+                            maxLength: 2,
+                            style: TextStyle(color: Colors.white),
+                            onSubmitted: (_) {
+                              _pageController.nextPage(
+                                  duration: Duration(milliseconds: 200),
+                                  curve: Curves.decelerate);
+                            },
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              InnerPageUserData(
+                children: <Widget>[
+                  FlatButton(
+                    color: secondaryColor,
+                    child: Text("Create Video", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+                    onPressed: () {},
+                  )
+                ],
+              )
             ],
           ),
           Align(
@@ -133,6 +255,7 @@ class _CreateProfileState extends State<CreateProfile> {
                   _pageController.previousPage(
                       duration: Duration(milliseconds: 200),
                       curve: Curves.decelerate);
+                  FocusScope.of(context).requestFocus(FocusNode());
                 }),
           ),
         ],
@@ -143,6 +266,7 @@ class _CreateProfileState extends State<CreateProfile> {
         onPressed: () {
           _pageController.nextPage(
               duration: Duration(milliseconds: 200), curve: Curves.decelerate);
+          FocusScope.of(context).requestFocus(FocusNode());
         },
       ),
     );
