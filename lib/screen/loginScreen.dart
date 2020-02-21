4217ff57b2bc1615c6013e114789c4cfc351bbbc
Kevin Friedl <kevin.friedl@adorsys.de>
2020-02-21 18:54:20 +0100

    edit login screen

--- lib/screen/loginScreen.dart
+++ lib/screen/loginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:video_match/screen/homeScreen.dart';
+import 'package:video_match/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -10,7 +12,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  
+
   Future<FirebaseUser> _handleSignIn() async {
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth =
@@ -29,17 +31,38 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return VMScaffold(
+      colorfulBackground: true,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset("assets/app_icon.png"),
-            FlatButton(
-              child: Text("Sign in with Google"),
-              onPressed: () {
-                _handleSignIn();
-              },
+            Text("Welcome!",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Divider(
+              color: Colors.transparent,
+              height: 30,
+            ),
+            CircleAvatar(
+              child: Image.asset("assets/app_icon.png"),
+              radius: 100,
+              backgroundColor: Colors.white,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: FlatButton(
+                color: secondaryColor,
+                child: Text(
+                  "Sign in with Google",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                onPressed: () {
+                  _handleSignIn();
+                },
+              ),
             )
           ],
         ),
