d3325f23db52c65b6b2ff29a0eedc4add3a6839c
Kevin Friedl <kevin.friedl@adorsys.de>
2020-03-19 20:24:36 +0100

    add chat notificatinos

--- lib/utils/background/worker.dart
+++ lib/utils/background/worker.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:video_match/utils/server/server.dart';
 
@@ -8,40 +9,60 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 NotificationDetails platformChannelSpecifics;
 
 Future<void> worker() async {
-  List<String> likedUsers = List<String>();
+  List<String> usersLiked = List<String>();
   bool init = false;
 
   try {
     initNotifications();
     Server.instance.signIn().then((signedIn) {
-      if (signedIn)
-        Server.instance.likesProfileList().listen((data) async {
+      if (signedIn) {
+        Server.instance.likesProfileList().listen((dataLikedUser) async {
           if (init) {
-            data.documents.forEach((document) async {
-              if (!likedUsers.contains(document.documentID)) {
-                likedUsers.add(document.documentID);
-                await flutterLocalNotificationsPlugin.show(
-                    Random.secure().nextInt(2000),
-                    (Server.instance
-                            .checkOwnUserLikedBack(document.documentID, true))
-                        ? "Match!"
-                        : "New like!",
-                    "with " + document.data["name"],
-                    platformChannelSpecifics,
-                    payload: document.documentID);
+            dataLikedUser.documents.forEach((documentLikedUser) async {
+              if (!usersLiked.contains(documentLikedUser.documentID)) {
+                bool likedBack = Server.instance
+                    .checkOwnUserLikedBack(documentLikedUser.documentID, true);
+                usersLiked.add(documentLikedUser.documentID);
+                if (likedBack) chatMessageStream(documentLikedUser);
+                showNotification(likedBack ? "Match!" : "New like!",
+                    "with " + documentLikedUser.data["name"]);
               }
             });
           } else {
-            data.documents.forEach((document) {
-              likedUsers.add(document.documentID);
-            });
             init = true;
+            dataLikedUser.documents.forEach((documentLikedUser) {
+              usersLiked.add(documentLikedUser.documentID);
+              if (Server.instance
+                  .checkOwnUserLikedBack(documentLikedUser.documentID, true)) {
+                chatMessageStream(documentLikedUser);
+              }
+            });
           }
         });
+      }
     });
   } catch (e) {
     print(e.toString());
   }
+}
+
+chatMessageStream(DocumentSnapshot documentLikedUser) {
+  bool chatInit = false;
+  Server.instance.chatStream(documentLikedUser.documentID).listen((dataChat) {
+    if (dataChat.documents.first.data["messages"].last.keys.first !=
+            Server.instance.firebaseUser.uid.substring(0, 6) &&
+        chatInit) {
+      showNotification(documentLikedUser.data["name"],
+          dataChat.documents.first.data["messages"].last["m"].toString());
+    } else
+      chatInit = true;
+  });
+}
+
+showNotification(String title, String message) async {
+  await flutterLocalNotificationsPlugin.show(
+      Random.secure().nextInt(2000), title, message, platformChannelSpecifics,
+      payload: null);
 }
 
 initNotifications() async {
